6d396c726eaafe3817e482b8c8d4d86fcf087738
Ahmad Yasin <ahmad.yasin@intel.com>
2020-09-01 00:19:09 +0300

    Revert "An extensible generator of x86 codes with a sample BACLEARs kernel"
    This reverts commit 229331bac625b41fce90395d99a5d6386f8baabd.

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Author: Ahmad Yasin
-# Aug. 2020
-#
-./gen-kernel.py jumpy -n 5 -i JMP -a 14  > jumpy5p14.c
-gcc -S jumpy5p14.c
-gcc -o jumpy5p14 jumpy5p14.c
-#perf stat -a --topdown -e instructions,BACLEARS.ANY --no-metric-only -C0 -- taskset 0x1 ./jumpy5p14 999999999

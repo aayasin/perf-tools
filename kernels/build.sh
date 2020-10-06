19fa247c2116a3a5b4f2f6879542c763ab8e5d9e
Ahmad Yasin <ahmad.yasin@intel.com>
2020-10-06 13:43:25 +0300

    New kernel for Mixing_Vectors node in TMA. The generator now can take multiple instructions for the loop-body.

--- kernels/build.sh
+++ kernels/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # Author: Ahmad Yasin
-# Aug. 2020
+# Oct. 2020
 #
 #set -x
 ./gen-kernel.py jumpy -n 5 -i JMP -a 14  > jumpy5p14.c
 gcc -S jumpy5p14.c
 gcc -o jumpy5p14 jumpy5p14.c
 #perf stat -a --topdown -e instructions,BACLEARS.ANY --no-metric-only -C0 -- taskset 0x1 ./jumpy5p14 999999999
+./gen-kernel.py -i "addps %xmm1,%xmm2" "vsubps %ymm1,%ymm2,%ymm3" -n10 > sse2avx.c
+gcc -o sse2avx sse2avx.c

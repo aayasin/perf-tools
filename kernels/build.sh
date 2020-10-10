9c9afcee168ad2e677eeb0011e5e72b197d79e1e
Ahmad Yasin <ahmad.yasin@intel.com>
2020-10-11 00:20:37 +0300

    Compiler with -g -O2

--- kernels/build.sh
+++ kernels/build.sh
@@ -2,10 +2,13 @@
 # Author: Ahmad Yasin
 # Oct. 2020
 #
-#set -x
+CC='gcc -g -O2'
+set -x
+
 ./gen-kernel.py jumpy -n 5 -i JMP -a 14  > jumpy5p14.c
 gcc -S jumpy5p14.c
-gcc -o jumpy5p14 jumpy5p14.c
+$CC -o jumpy5p14 jumpy5p14.c
 #perf stat -a --topdown -e instructions,BACLEARS.ANY --no-metric-only -C0 -- taskset 0x1 ./jumpy5p14 999999999
+
 ./gen-kernel.py -i "addps %xmm1,%xmm2" "vsubps %ymm1,%ymm2,%ymm3" -n10 > sse2avx.c
-gcc -o sse2avx sse2avx.c
+$CC -o sse2avx sse2avx.c

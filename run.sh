439c2486f6da92f8d4641f927111bf122896d78d
Ahmad Yasin <ahmad.yasin@intel.com>
2020-10-16 01:44:47 +0300

    Advising to print execution-time and work metric Documenting each step in the runner.

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# replace next line with invocation to your application + arguments below.
+# replace the following with invocation to your application + arguments below.
 
-taskset 0x3 ./pmu-tools/workloads/PYTHON1s		#a sample command to be replaced
-printf "\n\tDid you missed to add your cmd?!\n"		#once done, comment out this line
+# taskset set CPU affinity for consistent measurements (for CPUs 0,1 in tnis example)
+taskset 0x3 \
+ ./pmu-tools/workloads/PYTHON1s 2>&1 | 	# a sample command with no arguments
+ tee out.log |				# redirect bulk output to some log file
+ grep seconds				# grep for execution-time & some work metric
+ 
+# sample output of the last command
+# "Game 0 played in 37.6 seconds with 24 moves"
 
-# consider CPU affinity for consistent measurements
-# redirect bulk output to some log file
-
+printf "\n\tDid you added your cmd?!\n"	# remove me

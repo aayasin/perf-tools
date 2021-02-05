1195e0c0503bcadbe759f05a1d0afc573d915666
Ahmad Yasin <ahmad.yasin@intel.com>
2021-02-05 22:06:36 +0200

    New rfetch64k kernel: a 64KB binary with random cyclic jumps!

--- kernels/build.sh
+++ kernels/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Author: Ahmad Yasin
-# Dec. 2020
+# Feb. 2021
 #
 CC='gcc -g -O2'
 set -x
 
-./gen-kernel.py jumpy -n 5 -i JMP -a 14  > jumpy5p14.c
+./gen-kernel.py jumpy-seq -n 5 -i JMP -a 14  > jumpy5p14.c
 $CC -S jumpy5p14.c
 #$CC -o jumpy5p14 jumpy5p14.c
 #perf stat -a --topdown -e instructions,BACLEARS.ANY --no-metric-only -C0 -- taskset 0x1 ./jumpy5p14 999999999
@@ -14,8 +14,9 @@ $CC -S jumpy5p14.c
 #$CC -o sse2avx sse2avx.c
 
 ./gen-kernel.py -i NOP 'test %rax,%rax' 'jle Lbl_end' -n 1 -a 6 > peak4wide.c
-./gen-kernel.py jumpy -i JL -a 6 -n 20000  > jcc20k.c
+./gen-kernel.py jumpy-seq -i JL -a 6 -n 20000  > jcc20k.c
+./gen-kernel.py jumpy-random -a 6 -i JMP -n 1024 > rfetch64k.c
 
-for x in jumpy5p14 jcc20k sse2avx peak4wide; do
+for x in rfetch64k jumpy5p14 jcc20k sse2avx peak4wide; do
   $CC -o $x $x.c
 done

9dd44479caa9d37331119e76b763394e344af170
Ahmad Yasin <ahmad.yasin@intel.com>
2021-02-26 00:07:34 +0200

    Options to specific prolog/epilog instructions arround the main loop

--- kernels/gen-kernel.py
+++ kernels/gen-kernel.py
@@ -3,7 +3,7 @@
 # Author: Ahmad Yasin
 # edited: Feb. 2021
 __author__ = 'ayasin'
-__version__ = 0.5
+__version__ = 0.51
 # TODO:
 # - multi-byte NOP support
 # - move Paper to a seperate module
@@ -25,15 +25,20 @@ ap = argparse.ArgumentParser()
 ap.add_argument('-n', '--num', type=int, default=3, help='# times to repeat instruction(s), aka unroll-factor')
 ap.add_argument('-r', '--registers', type=int, default=0, help="# of registers to traverse via '@' if > 0")
 ap.add_argument('--registers-max', type=int, default=16, help="max # of registers in the instruction-set")
-ap.add_argument('-i', '--instruction', nargs='+', default=[INST_UNIQ])
+ap.add_argument('-i', '--instructions', nargs='+', default=[INST_UNIQ])
+ap.add_argument('-p', '--prolog-instructions', nargs='+', default=[], help='instructions prior to the primary loop')
+ap.add_argument('-e', '--epilog-instructions', nargs='+', default=[], help='instructions post  the primary loop')
 ap.add_argument('-a', '--align' , type=int, default=0, help='in power of 2')
 ap.add_argument('-o', '--offset', type=int, default=0)
 ap.add_argument('--label-prefix', default='Lbl')
 ap.add_argument('mode', nargs='?', choices=['basicblock']+J.jumpy_modes, default='basicblock')
 args = ap.parse_args()
 
-if args.registers > 0 and not '@' in ' '.join(args.instruction): sys.exit("expect ''@' in instruction")
-paper = '"Reference: %s"'%Papers['MGM'] if args.registers > 0 else str(0)
+paper=str(0)
+if args.registers > 0:
+  if not '@' in ' '.join(args.instructions): sys.exit("expect '@' in --instructions")
+  if args.registers > args.registers_max:   sys.exit("invalid value for --registers! must be < %d"%args.registers_max)
+  paper='"Reference: %s"'%Papers['MGM']
 
 def asm(x, tabs=1, spaces=8):
   if x == 'MOVLG': x = MOVLG
@@ -60,7 +65,7 @@ int main(int argc, const char* argv[])
     }
     if (MSG) printf("%%s\\n", MSG ? MSG : "");
     n= atol(argv[1]);"""%paper
-asm(INST_UNIQ, spaces=4)
+for inst in [INST_UNIQ] + args.prolog_instructions: asm(inst, spaces=4)
 if args.align: asm('.align %d'%(2 ** args.align), tabs=0)
 print "    for (i=0; i<n; i++) {"
 for j in range(args.num):
@@ -68,7 +73,7 @@ for j in range(args.num):
      for k in range(j+args.offset-1): asm(INST_1B)
   if jumpy(): asm(label(j), tabs=0)
   for r in range(max(args.registers, 1)):
-    for inst in args.instruction:
+    for inst in args.instructions:
       if inst in ['JMP', 'JL']: inst += label(J.next(args.mode, args.num), False)
       if args.registers and '@' in inst:
         for i in range(9):
@@ -78,9 +83,11 @@ for j in range(args.num):
       asm(inst)
   if jumpy() and args.align: asm('.align %d'%(2 ** args.align), tabs=0)
 if jumpy(): asm(label(args.num), tabs=0)
+print "    }"
 
-print """    }
-    asm(".align 512; %s_end:");
+for inst in args.epilog_instructions: asm(inst, spaces=4)
+
+print """    asm(".align 512; %s_end:");
 
     return 0;
 }"""%args.label_prefix

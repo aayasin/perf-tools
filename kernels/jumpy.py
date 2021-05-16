c871c7bec72f4ffb220853d6e881bfb734ba5b77
Ahmad Yasin <ahmad.yasin@intel.com>
2021-05-16 03:41:04 +0300

    ported code to python3

--- kernels/jumpy.py
+++ kernels/jumpy.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # Author: Ahmad Yasin
-# edited: Feb. 2021
+# edited: May. 2021
+from __future__ import print_function
 __author__ = 'ayasin'
 
 import random
@@ -23,34 +24,34 @@ def jumpy_idx(mode, n):
       if n < 4: C.error('jumpy-random: cannot converge with --num<4')
       done = False
       visited_l = [0] * n
-      if debug: print visited_l 
+      if debug: print(visited_l)
       step('// jump-random: trials ')
       xx=n
       trial=1
       while not done:
         jumpy_idx.list = random.sample(range(n), n)
         while jumpy_idx.list[0] in [n-1, 1]:
-          if debug: print 'list[0]=%d:'%jumpy_idx.list[0], jumpy_idx.list
+          if debug: print('list[0]=%d:'%jumpy_idx.list[0], jumpy_idx.list)
           step()
           jumpy_idx.list = random.sample(range(n), n)
           trial += 1
         visited_l = [0] * n
         done = True
-        if debug: print 'while:', jumpy_idx.list
+        if debug: print('while:', jumpy_idx.list)
         x=0
         r=0
         while r < n:
           if visited_l[x]:
             done = False
-            if debug: print 'cycle of r=%d!'%r
+            if debug: print('cycle of r=%d!'%r)
             break
           visited_l[x]=1
           xx = x
           x = jumpy_idx.list[x]
           r += 1
-        if debug and done: print 'done: xx=%d, r=%d!'%(xx, r)
+        if debug and done: print('done: xx=%d, r=%d!'%(xx, r))
       jumpy_idx.list[xx] = n
-      if debug: print "final:", jumpy_idx.list
+      if debug: print("final:", jumpy_idx.list)
       step('\n')
       jumpy_idx.counter = -1
     return jumpy_idx.list.pop(0)

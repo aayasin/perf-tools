4bcd9bb76eb26fad33857036c93203b370753f6b
Ahmad Yasin <ahmad.yasin@intel.com>
2021-06-15 23:46:05 +0300

    use python3 unless overwridden by PY env var

--- kernels/build.sh
+++ kernels/build.sh
@@ -4,25 +4,26 @@
 #
 CC='gcc -g -O2'
 GEN=${GEN:-1}
+PY=${PY:-python3}
 
 set -xe
 
 if [ $GEN -eq 1 ]; then
-./gen-kernel.py jumpy-seq -n 5 -i JMP -a 14  > jumpy5p14.c
+$PY ./gen-kernel.py jumpy-seq -n 5 -i JMP -a 14  > jumpy5p14.c
 $CC -S jumpy5p14.c
 #$CC -o jumpy5p14 jumpy5p14.c
 #perf stat -a --topdown -e instructions,BACLEARS.ANY --no-metric-only -C0 -- taskset 0x1 ./jumpy5p14 999999999
 
-./gen-kernel.py -i "addps %xmm1,%xmm2" "vsubps %ymm1,%ymm2,%ymm3" -n10 > sse2avx.c
+$PY ./gen-kernel.py -i "addps %xmm1,%xmm2" "vsubps %ymm1,%ymm2,%ymm3" -n10 > sse2avx.c
 #$CC -o sse2avx sse2avx.c
 
-./gen-kernel.py -i NOP 'test %rax,%rax' 'jle Lbl_end' -n 1 -a 6 > peak4wide.c
-./gen-kernel.py -i NOP NOP 'test %rax,%rax' 'jle Lbl_end' -n 1 -a 6 > peak5wide.c
-./gen-kernel.py jumpy-seq -i JL -a 6 -n 20000  > jcc20k.c
-./gen-kernel.py jumpy-random -a 6 -i JMP -n 1024 > rfetch64k.c
-./gen-kernel.py jumpy-random -a 6 -i JMP -n 49152 > rfetch3m.c
-./gen-kernel.py -i 'vaddpd %ymm@,%ymm@,%ymm@' -r16 -n1 > fp-add-bw.c
-./gen-kernel.py -i 'vaddpd %ymm@-1,%ymm@,%ymm@' -r16 -n1 > fp-add-lat.c
+$PY ./gen-kernel.py -i NOP 'test %rax,%rax' 'jle Lbl_end' -n 1 -a 6 > peak4wide.c
+$PY ./gen-kernel.py -i NOP NOP 'test %rax,%rax' 'jle Lbl_end' -n 1 -a 6 > peak5wide.c
+$PY ./gen-kernel.py jumpy-seq -i JL -a 6 -n 20000  > jcc20k.c
+$PY ./gen-kernel.py jumpy-random -a 6 -i JMP -n 1024 > rfetch64k.c
+$PY ./gen-kernel.py jumpy-random -a 6 -i JMP -n 49152 > rfetch3m.c
+$PY ./gen-kernel.py -i 'vaddpd %ymm@,%ymm@,%ymm@' -r16 -n1 > fp-add-bw.c
+$PY ./gen-kernel.py -i 'vaddpd %ymm@-1,%ymm@,%ymm@' -r16 -n1 > fp-add-lat.c
 fi
 
 ks="fp-add-{bw,lat},jcc20k,jumpy5p14,memcpy,peak[45]wide,rfetch{64k,3m},sse2avx"

dc51f0d9dff56634b95eafd6fc8d56d28b3fa391
Ahmad Yasin <ahmad.yasin@intel.com>
2021-09-05 16:37:04 +0300

    seperate out.log file per each run

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # replace the following with invocation to your application + arguments below.
 
-# taskset set CPU affinity for consistent measurements (for CPUs 0,1 in tnis example)
+# taskset set CPU affinity for consistent measurements (for CPUs 0,1 in this example)
 taskset 0x3 \
  ./pmu-tools/workloads/PYTHON1s 2>&1 | 	# a sample command with no arguments
- tee out.log |				# redirect bulk output to some log file
+ tee .out-$1-$$.log |			# redirect bulk output to some log file
  grep seconds				# grep for execution-time & some work metric
  
 # sample output of the last command

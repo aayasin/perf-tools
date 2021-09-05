7b4ededa3fa23c81beaa587fa0761fbda0f95855
Ahmad Yasin <ahmad.yasin@intel.com>
2021-09-05 15:56:41 +0300

    new peak6wide kernel (Alder Lake)

--- kernels/build.sh
+++ kernels/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Author: Ahmad Yasin
-# Jul. 2021
+# Sep. 2021
 #
 CC=${CC:-gcc -g -O2}
 GEN=${GEN:-1}
@@ -20,6 +20,7 @@ $PY ./gen-kernel.py -i "addps %xmm1,%xmm2" "vsubps %ymm1,%ymm2,%ymm3" -n10 > sse
 
 $PY ./gen-kernel.py -i NOP 'test %rax,%rax' 'jle Lbl_end' -n 1 -a 6 > peak4wide.c
 $PY ./gen-kernel.py -i NOP NOP 'test %rax,%rax' 'jle Lbl_end' -n 1 -a 6 > peak5wide.c
+$PY ./gen-kernel.py -i NOP#3 'test %rax,%rax' 'jle Lbl_end' -n 1 -a 6 > peak6wide.c
 $PY ./gen-kernel.py jumpy-seq -i JG -a 6 -n 20000  > jcc20k.c
 $PY ./gen-kernel.py jumpy-random -a 6 -i JMP -n 1024 > rfetch64k.c
 $PY ./gen-kernel.py jumpy-random -a 6 -i JMP -n 49152 > rfetch3m.c
@@ -29,7 +30,7 @@ $PY ./gen-kernel.py -i 'vmulpd %ymm@,%ymm@,%ymm@' -r16 -n1 > fp-mul-bw.c
 $PY ./gen-kernel.py -i 'vmulpd %ymm@-1,%ymm@,%ymm@' -r16 -n10 > fp-mul-lat.c
 fi
 
-ks="fp-{add,mul}-{bw,lat},jcc20k,jumpy*,memcpy,peak[45]wide,rfetch{64k,3m},sse2avx"
+ks="fp-{add,mul}-{bw,lat},jcc20k,jumpy*,memcpy,peak*,rfetch{64k,3m},sse2avx"
 kernels=`bash -c "ls {$ks}.c | sed 's/\.c//'"`
 for x in $kernels; do
   $CC -o $x $x.c

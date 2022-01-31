ce9f4071e75213d708904cc1d0e584841bf6fa75
Ahmad Yasin <ahmadyasin79@gmail.com>
2022-01-31 14:23:11 -0800

    rfetch3m-ic: new rfetch kernel with icache-misses only Tuned the current rfetch3m to reduce # BAClears

--- kernels/build.sh
+++ kernels/build.sh
@@ -23,7 +23,8 @@ $PY ./gen-kernel.py -i NOP NOP 'test %rax,%rax' 'jle Lbl_end' -n 1 -a 6 > peak5w
 $PY ./gen-kernel.py -i NOP#3 'test %rax,%rax' 'jle Lbl_end' -n 1 -a 6 > peak6wide.c
 $PY ./gen-kernel.py jumpy-seq -i JG -a 6 -n 20000  > jcc20k.c
 $PY ./gen-kernel.py jumpy-random -a 6 -i JMP -n 1024 > rfetch64k.c
-$PY ./gen-kernel.py jumpy-random -a 6 -i JMP -n 49152 > rfetch3m.c
+$PY ./gen-kernel.py jumpy-random -a 5 -i NOP5#30 JMP -n 19661 > rfetch3m.c
+$PY ./gen-kernel.py jumpy-random -a 5 -i NOP5#56 JMP -n 10923 > rfetch3m-ic.c
 for x in add mul; do
   $PY ./gen-kernel.py -i "v${x}pd %ymm@,%ymm@,%ymm@" -r16 -n1 --reference MGM > fp-$x-bw.c
   $PY ./gen-kernel.py -i "v${x}pd %ymm@-1,%ymm@,%ymm@" -r16 -n10 --reference MGM > fp-$x-lat.c
@@ -31,7 +32,7 @@ done
 $PY ./gen-kernel.py -i 'vfmadd132pd %ymm10,%ymm11,%ymm11' 'vfmadd132ps %ymm12,%ymm13,%ymm13' 'vaddpd %xmm0,%xmm0,%xmm0' 'vaddps %xmm1,%xmm1,%xmm1' 'vsubsd %xmm2,%xmm2,%xmm2' 'vsubss %xmm3,%xmm3,%xmm3' -n1 --reference 'ICL-PMU' > fp-arith-mix.c
 fi
 
-ks="fp-{{add,mul}-{bw,lat},arith-mix},jcc20k,jumpy*,memcpy,peak*,rfetch{64k,3m},sse2avx"
+ks="fp-{{add,mul}-{bw,lat},arith-mix},jcc20k,jumpy*,memcpy,peak*,rfetch{64k,3m{,-ic}},sse2avx"
 kernels=`bash -c "ls {$ks}.c | sed 's/\.c//'"`
 for x in $kernels; do
   $CC -o $x $x.c

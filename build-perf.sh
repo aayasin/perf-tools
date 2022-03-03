ceab3d838876326b1fdb02304a87252aebb2a7e2
Ahmad Yasin <ahmadyasin79@gmail.com>
2022-03-02 23:41:52 -0800

    added control if to download perf tool tar and what version

--- build-perf.sh
+++ build-perf.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
+CLONE=${CLONE:-1}
+PERFV=${PERFV:-5.15.0}
+
+perfdir=linux/tools/perf/
 set -xe
-git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
-# if previous command fails, try this alternative :
-# wget https://mirrors.edge.kernel.org/pub/linux/kernel/tools/perf/v5.14.0/perf-5.14.0.tar.xz && tar -xvf ./perf-5.14.0.tar.xz
-cd ./linux/tools/perf/
+if [ $CLONE -eq 1 ]; then
+  git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+else
+  # if previous command fails, try this alternative :
+  wget https://mirrors.edge.kernel.org/pub/linux/kernel/tools/perf/v$PERFV/perf-$PERFV.tar.xz && tar -xvf ./perf-$PERFV.tar.xz
+  perfdir=perf-$PERFV/tools/perf
+fi
+
+cd $perfdir
 sudo apt-get install -y flex
 sudo apt-get install -y bison
 sudo apt-get install libslang2-dev
+#sudo apt-get install libiberty-dev libzstd-dev #demangle
 #sudo apt-get install libelf
 sudo apt-get install make
 #warning: next line was tested on Ubuntu

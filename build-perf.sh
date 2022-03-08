23a3054e112d69800c2aa2649470f6bb3a675d13
aayasin <ahmadyasin79@gmail.com>
2022-03-08 18:13:38 +0200

    comment out libelf for now (fails on Ubuntu)

--- build-perf.sh
+++ build-perf.sh
@@ -7,7 +7,7 @@ cd ./linux/tools/perf/
 sudo apt-get install -y flex
 sudo apt-get install -y bison
 sudo apt-get install libslang2-dev
-sudo apt-get install libelf
+#sudo apt-get install libelf
 sudo apt-get install make
 #warning: next line was tested on Ubuntu
 sudo apt-get install -y libbfd-dev libdwarf-dev libelf-dev

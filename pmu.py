500c2f38e086ad87bd7451a26e6b1370c6238e4f
Ahmad Yasin <ahmadyasin79@gmail.com>
2022-04-20 19:02:56 -0700

    linting fixes

--- pmu.py
+++ pmu.py
@@ -16,19 +16,19 @@ def name():
   f += '/caps/pmu_name'
   return C.file2str(f) or 'Unknown PMU'
 
-#per CPU PMUs
+# per CPU PMUs
 def skylake():    return name() == 'skylake'
 def icelake():    return name() == 'icelake'
 def alderlake():  return name() == 'alderlake_hybrid'
 def sapphire():   return name() == 'sapphire_rapids'
 
-#aggregations
+# aggregations
 def goldencove():   return alderlake() or sapphire()
 def perfmetrics():  return icelake() or goldencove()
-#Icelake onward PMU, e.g. Intel PerfMon Version 5+
+# Icelake onward PMU, e.g. Intel PerfMon Version 5+
 def v5p(): return perfmetrics()
 
-#events
+# events
 def lbr_event():
   return ('cpu_core/event=0xc4,umask=0x20/' if alderlake() else 'r20c4:') + 'ppp'
 
@@ -40,15 +40,15 @@ def cpu_has_feature(feature):
   return feature in flags
 
 def cpu(what):
-  if 1: #not cpu.state:
+  if 1:  # not cpu.state:
     sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/pmu-tools')
     import tl_cpu
     cpu_state = tl_cpu.CPU((), False, tl_cpu.Env())
   return {'smt-on': cpu_state.ht}[what]
-#cpu.state = None
+# cpu.state = None
 
-def cpu_peak_kernels(widths=range(4,7)):
-  return ['peak%dwide'%x for x in widths]
+def cpu_peak_kernels(widths=range(4, 7)):
+  return ['peak%dwide' % x for x in widths]
 
 def cpu_pipeline_width():
   width = 4
@@ -65,6 +65,6 @@ def dsb_msb():
 def dsb_set_index(ip):
   left = dsb_msb()
   if left:
-    mask = 2**(left+1)-1
+    mask = 2 ** (left + 1) - 1
     return ((ip & mask) >> 6)
   return None

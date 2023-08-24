055f1443274205f2a733d3ad98bf27f0fb949609
Ahmad Yasin <ahmadyasin79@gmail.com>
2023-08-25 02:06:48 +0300

    study.py enhanced with dsb-align mode & a tar stage

--- common.py
+++ common.py
@@ -322,7 +322,7 @@ def argument_parser(usg, defs=None, mask=PROF_MASK_DEF, fc=argparse.ArgumentDefa
     '2:+verbose-on metrics|build|sub-commands, 3:+toplev --perf|ASM on kernel build|greedy lbr.py, 4:+args parsing, '
     '5:+event-groups, 6:ocperf verbose, .. 9:anything')
   add_hex_arg(ap, '-pm', '--profile-mask', mask, 'stages in the profile command. See profile-mask-help.md for details')
-  ap.add_argument('--tune', nargs='+', help=argparse.SUPPRESS, action='append') # override global variables with python expression
+  ap.add_argument('--tune', nargs='+', default=common_def('tune'), help=argparse.SUPPRESS, action='append') # override global variables with python expression
   return ap
 
 def commands_list():

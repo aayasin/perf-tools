f383f675b2cee0341aaa2ff322f96e3284837229
Ahmad Yasin <ahmadyasin79@gmail.com>
2024-10-15 14:46:03 -0700

    Revision 3:with comments

--- pipeline.py
+++ pipeline.py
@@ -17,6 +17,7 @@ def pipeline_view(log, depths):
   event_list = []
   depth_list = []
   lines = []
+  #Get the events and the depths of each uarch structure [ dsb, mite, decoders, ms, issue, execute, retire ]
   for i in depths:
     event_list.append(depths[i][0])
     depth_list.append(int(depths[i][1]))
@@ -30,29 +31,39 @@ def pipeline_view(log, depths):
   percent_chunks = get_empty_chunk_list(rows, event_list, depth_list)
   final_percent = [[[0 for i in range(k)] for j, k in zip(range(len(event_list)), depth_list)] for l in range(rows)]
   totals = [[0 for i in range(len(event_list))] for j in range(rows)]
+  #Iterate through the list of events and their associated depths
   for event_name, event_depth in zip(event_list, depth_list):
     sum = 0
     lines = []
     index = event_list.index(str(event_name))
     cols = event_depth
     lines = get_search_list(pipe_list, event_name)
+    #Comb through the perf-stats csv and grep for the events and put it into chunks list
     chunks[index] = [[lines[i * cols + j] for j in range(cols)] for i in range(rows)]
     for i in range(rows):
       for j in range(event_depth-1):
-        if not "not" in chunks[index][i][j] and not "not" in chunks[index][i][j+1]:
+        #Take the different between cmask-N which is the superset and cmask-N+1 which is the subset put that into the processed_chunks lists
+        if not "not" in chunks[index][i][j] and not "not" in chunks[index][i][j+1]: 
+          #This takes into accout the PMU queury rate, and the possibility that an event with cmask N can be possibly smaller than N+1 (superset < subset) due to shift in workload intensity, so we take the abs(absolute value).
           processed_chunks[index][i][j]=abs(int(chunks[index][i][j])-int(chunks[index][i][j+1]))
         else:
+           #Error check that "not counted" doesn't exist 
            common.error("Some events were not counted, try running application longer; aborting")
-        if j == (cols-2):
+        #Put the tail cmask as its own calculation outside of loop.
+        if j == (cols-2): 
           processed_chunks[index][i][j+1] = int(chunks[index][i][j+1])
+      #Generate a sum value as total sum of an event to be divisor.
       for k in range(cols):
         sum += processed_chunks[index][i][k]
+      #Use the sum to convert discrete values into percents and place into percent_chunks list
       for l in range(cols):
         percent_chunks[index][i][l] = int(100*processed_chunks[index][i][l]/sum)
       totals[i][index] = sum
       sum = 0
+  #Convert the percent_chunks from homogenous event data to heterogenous data where dsb, mite, decoders, ms, issue, execute, retire all occupy a single snapshot.
   final_percent = [[[percent_chunks[j][i][l] for l in range(k)] for j, k in zip(range(len(event_list)), depth_list)] for i in range(rows)]
   time = 0
+  #Use the imported tabulate to post-process and write to output file. We anchor all events to UOPS ISSUED.
   for i in range(rows):
     subtotals = totals[i]
     headers = [event_name.upper()+': '+str(round(subtotal/1000/1000,2))+'M | '+str(round(subtotal/subtotals[issued_index]*100,2))+'% of Issue' for event_name, subtotal in zip(event_list, subtotals)]
